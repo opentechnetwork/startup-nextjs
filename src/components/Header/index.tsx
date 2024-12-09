852917d24ebf89d8ee7ca851ad0d3239a0948f77
opentechnetwork <149030482+opentechnetwork@users.noreply.github.com>
2024-12-08 23:27:44 -0600

    Updates
    Added a dark image for light background.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,7 +57,7 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/otin.svg"
+                  src="/images/logo/otin-dark.svg"
                   alt="logo"
                   width={140}
                   height={30}
