a5cb2ef3a0dcf5ad7bac38cce1733b029e8f9415
opentechnetwork <149030482+opentechnetwork@users.noreply.github.com>
2024-12-08 19:23:15 -0600

    next.config.js
    Fixed a deprecated from of code images.domains no longer works and breaks the site.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
         port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000", // Replace with the port your localhost is running on, if applicable
       },
     ],
   },
