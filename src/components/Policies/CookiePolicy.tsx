35c189fb7791ae5edb3c9844f0783bae9b377aee
opentechnetwork <149030482+opentechnetwork@users.noreply.github.com>
2025-01-10 18:53:54 -0600

    Policy Updates
    Cookies, and disclaimer.

--- src/components/Policies/CookiePolicy.tsx
+++ src/components/Policies/CookiePolicy.tsx
@@ -3,44 +3,83 @@ import React from "react";
 const CookiePolicy = () => {
   const currentDate = new Date().toLocaleDateString();
   return (
-    <section className="py-12 px-6">
-      <h1 className="text-3xl font-bold mb-4">Cookie Policy</h1>
-      <p className="text-lg mb-8">Effective Date: {currentDate}</p>
+    <section className="px-6 py-12">
+      <h1 className="mb-4 text-3xl font-bold">Cookie Policy</h1>
+      <p className="mb-8 text-lg">Effective Date: {currentDate}</p>
 
       <p className="mb-4">
-        Open Tech Innovations uses cookies on [website URL]. By using the Service, you consent to the use of cookies.
+        Open Tech Innovations uses cookies on [website URL]. By using the
+        Service, you consent to the use of cookies.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">What Are Cookies</h2>
+      <h2 className="mb-2 text-2xl font-semibold">What Are Cookies</h2>
       <p className="mb-4">
-        Cookies are small text files that are used to store small pieces of information. They are stored on your device
-        when the website is loaded on
-        your browser. These cookies help us make the website function properly, make it more secure, provide better user
-        experience, and understand how the
-        website performs and to analyze what works and where it needs improvement.
+        Cookies are small text files that are used to store small pieces of
+        information. They are stored on your device when the website is loaded
+        on your browser. These cookies help us make the website function
+        properly, make it more secure, provide a better user experience, and
+        understand how the website performs and to analyze what works and where
+        it needs improvement. For more information, you can visit{" "}
+        <a
+          href="https://html.com/resources/cookies-ultimate-guide/"
+          className="text-blue-600 underline"
+          target="_blank"
+        >
+          HTML.com's Ultimate Guide to Cookies{" "}
+        </a>
+        or{" "}
+        <a
+          href="https://www.cloudflare.com/learning/privacy/what-are-cookies/"
+          className="text-blue-600 underline"
+          target="_blank"
+        >
+          Cloudflare's guide on what cookies are
+        </a>
+        .
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">Types of Cookies We Use</h2>
-      <ul className="list-disc pl-6 mb-4">
-        <li>Essential Cookies: These are necessary for the website to function and cannot be switched off in our
-          systems.
+      <h2 className="mb-2 text-2xl font-semibold">Types of Cookies We Use</h2>
+      <ul className="mb-4 list-disc pl-6">
+        <li>
+          Essential Cookies: These are necessary for the website to function and
+          cannot be switched off in our systems.
         </li>
-        <li>Analytics Cookies: These allow us to count visits and traffic sources so we can measure and improve the
-          performance of our site.
+        <li>
+          Analytics Cookies: These allow us to count visits and traffic sources
+          so we can measure and improve the performance of our site.
         </li>
       </ul>
 
-      <h2 className="text-2xl font-semibold mb-2">Managing Cookies</h2>
+      <h2 className="mb-2 text-2xl font-semibold">Managing Cookies</h2>
       <p className="mb-4">
-        You can set your browser to refuse all or some browser cookies, or to alert you when websites set or access
-        cookies. If you disable or refuse cookies,
-        please note that some parts of this site may become inaccessible or not function properly.
+        You can set your browser to refuse all or some browser cookies, or to
+        alert you when websites set or access cookies. If you disable or refuse
+        cookies, please note that some parts of this site may become
+        inaccessible or not function properly. For guidance, you can refer to {" "}
+        <a
+          href="https://support.google.com/chrome/answer/95647?hl=en&co=GENIE.Platform%3DDesktop"
+          className="text-blue-600 underline"
+          target="_blank"
+        >
+          Google's guide on managing cookies in Chrome
+        </a>
+        .
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">Contact Us</h2>
-      <p className="mb-4">If you have any questions about our cookie policy, please contact us at:</p>
+      <h2 className="mb-2 text-2xl font-semibold">Contact Us</h2>
+      <p className="mb-4">
+        If you have any questions about our cookie policy, please contact us at:
+      </p>
       <ul className="list-none pl-0">
-        <li>Email: support@openwebcommunications.com</li>
+        <li>
+          Email:{" "}
+          <a
+            href="mailto:support@openwebcommunications.com"
+            className="text-blue-600 underline"
+          >
+            support@openwebcommunications.com
+          </a>
+        </li>
       </ul>
     </section>
   );
--- src/components/Policies/Disclaimer.tsx
+++ src/components/Policies/Disclaimer.tsx
@@ -3,34 +3,67 @@ import React from "react";
 const Disclaimer = () => {
   const currentDate = new Date().toLocaleDateString(); // Get the current date
   return (
-    <section className="py-12 px-6">
-      <h1 className="text-3xl font-bold mb-4">Disclaimer</h1>
-      <p className="text-lg mb-8">Effective Date: {currentDate}</p>
+    <section className="px-6 py-12">
+      <h1 className="mb-4 text-3xl font-bold">Disclaimer</h1>
+      <p className="mb-8 text-lg">Effective Date: {currentDate}</p>
 
       <p className="mb-4">
-        The information provided by Open Tech Innovations on [website URL] is for general informational purposes only.
-        All information on the Site is provided in good faith; however, we make no representation or warranty of any
-        kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness
-        of any information on the Site.
+        The information provided by Open Tech Innovations on [website URL] is
+        for general informational purposes only. All information on the Site is
+        provided in good faith; however, we make no representation or warranty
+        of any kind, express or implied, regarding the accuracy, adequacy,
+        validity, reliability, availability, or completeness of any information
+        on the Site. For more information, you can refer to {" "}
+        <a
+          href="https://termly.io/resources/templates/disclaimer-template/"
+          className="text-blue-600 underline"
+          target="_blank"
+        >
+          Termly's Disclaimer Template
+        </a>
+        .
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">Professional Advice Disclaimer</h2>
+      <h2 className="mb-2 text-2xl font-semibold">
+        Professional Advice Disclaimer
+      </h2>
       <p className="mb-4">
-        The Site cannot and does not contain professional advice. The information is provided for general informational
-        and educational purposes only and is not a substitute for professional advice.
+        The Site cannot and does not contain professional advice. The
+        information is provided for general informational and educational
+        purposes only and is not a substitute for professional advice.
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">External Links Disclaimer</h2>
+      <h2 className="mb-2 text-2xl font-semibold">External Links Disclaimer</h2>
       <p className="mb-4">
-        The Site may contain links to other websites or content belonging to or originating from third parties. Such
-        external links are not investigated, monitored, or checked for accuracy, adequacy, validity, reliability,
-        availability, or completeness by us.
+        The Site may contain links to other websites or content belonging to or
+        originating from third parties. Such external links are not
+        investigated, monitored, or checked for accuracy, adequacy, validity,
+        reliability, availability, or completeness by us. To learn more about
+        handling external links and disclaimers, visit{" "}
+        <a
+          href="https://www.internetlegalattorney.com/when-is-website-disclaimer-effective/"
+          className="text-blue-600 underline"
+          target="_blank"
+        >
+          Phil Nicolosi Law
+        </a>
+        .
       </p>
 
-      <h2 className="text-2xl font-semibold mb-2">Contact Us</h2>
-      <p className="mb-4">If you have any questions about this Disclaimer, please contact us at:</p>
+      <h2 className="mb-2 text-2xl font-semibold">Contact Us</h2>
+      <p className="mb-4">
+        If you have any questions about this Disclaimer, please contact us at:
+      </p>
       <ul className="list-none pl-0">
-        <li>Email: support@openwebcommunications.com</li>
+        <li>
+          Email:{" "}
+          <a
+            href="mailto:support@openwebcommunications.com"
+            className="text-blue-600 underline"
+          >
+            support@openwebcommunications.com
+          </a>
+        </li>
       </ul>
     </section>
   );
