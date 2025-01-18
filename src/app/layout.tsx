f9f6b0cf10b4addfaa135805aa7e3ebf91b90cf3
opentechnetwork <149030482+opentechnetwork@users.noreply.github.com>
2025-01-18 16:53:48 -0600

    Blocking is working but need to make sure its going to the access denied page. Version5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 "use client";
-
+import { useState } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
 import "../styles/interfont.css";
+import "../styles/loading.css";
+import "../styles/googlefonts.css";
 import CookieConsentBanner from "@/components/CookieConsentBanner";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import "next/script";
 import { usePathname } from 'next/navigation';
+import Loading from "@/components/loading"; // Import the Loading component
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +22,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
 
+  const pathname = usePathname();
   // Exclude navigation and footer for specific routes
   const isIsolatedRoute = pathname === '/access-denied';
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingFinish = () => {
+    setIsLoading(false); // Hide the loading screen when complete
+  };
 
   return (
     <html suppressHydrationWarning lang="en">
@@ -51,7 +60,8 @@ export default function RootLayout({
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
           {!isIsolatedRoute && <Header />}
-          {children}
+          {isLoading && <Loading onFinish={handleLoadingFinish} />}
+          {!isLoading && children}
           <SpeedInsights />
           <Analytics />
           {!isIsolatedRoute && <Footer />}
