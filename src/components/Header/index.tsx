d63db9f9c296696e7294256c153f522a36048fff
opentechnetwork <149030482+opentechnetwork@users.noreply.github.com>
2025-01-30 16:44:25 -0600

    Adding the dark logo.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -64,7 +64,7 @@ const Header = () => {
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/otin.svg"
+                  src="/images/logo/otin-dark.svg"
                   alt="logo"
                   width={140}
                   height={30}
